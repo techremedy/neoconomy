cdacabc19d4875f62974137170d21990cc823610
techremedy <tom@techremedy.net>
2018-01-17 00:32:21 -0800

    creating json objects with asset data

--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -2,7 +2,18 @@ import { Template } from 'meteor/templating';
  
 import './body.html';
 
-
+if(Meteor.isServer){
+  Meteor.methods({
+    'getNEO': function () {
+      const data = HTTP.get('https://api.coinmarketcap.com/v1/ticker/neo/')
+      console.log(data);
+      return data;
+      }
+  })
+  
+  neo = Meteor.call('getNEO');
+  console.log(neo);
+}
  
 Template.body.helpers({
   assets: [
--- app/server/main.js
+++ app/server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import coinmarketcap from 'coinmarketcap';
 
 Meteor.startup(() => {
   // code to run on server at startup
