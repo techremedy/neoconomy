a188bbc73e8d0fcf59ff11a7ed8164decce8f8d6
techremedy <tom@techremedy.net>
2018-01-16 16:58:22 -0800

    preparing API call functions

--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -1,6 +1,15 @@
 import { Template } from 'meteor/templating';
  
 import './body.html';
+
+import { HTTP } from 'meteor/http';
+
+//array of assets to grab
+
+//function to get api data for given asset
+HTTP.get('https://api.coinmarketcap.com/v1/ticker/$ticker/', function (error, data){
+  console.log( 'http.get ::', error, data);
+});
  
 Template.body.helpers({
   assets: [
